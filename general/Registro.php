9ea38e3c3f6c15f771baf6883eff57e2124f0086
ricardo121 <ricardoasimbaya@gmail.com>
2018-02-01 11:00:46 +0100

    segunda carpeta

---
+++ general/Registro.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passing info with POST and HTML FORMS using a single file.</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Estilo.css"/>
+    <style>
+      span {
+        width: 100px;
+        display: inline-block;
+      }
+    </style>
+  </head>
+  <body>
+
+    <div class="container" id="contenedor" id="fondo" >
+
+
+
+      <?php if (!isset($_POST["Gmail"])) : ?>
+        <form method="post">
+          <fieldset>
+            <legend>Registarte</legend>
+            <span>Nombre:</span><input type="text" name="Nombre" required><br>
+            <span>Apellidos:</span><input type="text" name="Apellidos" required><br>
+            <span>Gmail:</span><input type="Email" name="Gmail" required><br>
+            <span>password:</span><input type="password" name="password" required><br>
+            <span>Edad:</span><input type="text" name="Edad"><br>
+            <p><input type="submit" value="Registrase"></p>
+          </fieldset>
+        </form>
+
+      <!-- DATA IN $_POST['mail']. Coming from a form submit -->
+      <?php else: ?>
+
+        <?php
+        //CREATING THE CONNECTION
+        $connection = new mysqli("localhost", "root", "Admin2015", "Proyecto",3316);
+        $connection->set_charset("uft8");
+        //TESTING IF THE CONNECTION WAS RIGHT
+        if ($connection->connect_errno) {
+            printf("Connection failed: %s\n", $connection->connect_error);
+            exit();
+        }
+        $Nom = $_POST["Nombre"];
+        $Ape = $_POST["Apellidos"];
+        $Em = $_POST["Email"];
+        $Pass =  md5($_POST["password"]);
+        $Edad = $_POST["Edad"];
+        $query = "INSERT INTO Usuarios(IdUsuario,Nombre,Apellidos,Gmail,password,Edad)
+        VALUES ('NULL','$Nom','$Ape','$Em','$Pass','$Edad')";
+        echo $query;
+        if ($connection->query($query)) {
+          echo "Se ha Registardo en ...";
+          $query ="SELECT * FROM Usuarios";
+          if ($result = $connection->query($query)) {
+            echo "<table>";
+
+
+            //FETCHING OBJECTS FROM THE RESULT SET
+            //THE LOOP CONTINUES WHILE WE HAVE ANY OBJECT (Query Row) LEFT
+
+            while($obj = $result->fetch_object()) {
+
+                //PRINTING EACH ROW
+                echo "<tr>";
+                  echo "<td>".$obj->IdUsuario."</td>";
+                  echo "<td>".$obj->Nombre."</td>";
+                  echo "<td>".$obj->Apellidos."</td>";
+                  echo "<td>".$obj->Edad."</td>";
+                  echo "<td>".$obj->password."</td>";
+                echo "</tr>";
+            }
+
+
+            echo "</table>";
+          }
+        } else {
+          echo "ERROR AL INSERTAR COCHE";
+        }
+        ?>
+
+      <?php endif ?>
+    </div>
+  </body>
+</html>
--- general/inicio.php
+++ general/inicio.php
@@ -1,40 +1,70 @@
+
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passing info with POST and HTML FORMS using a single file.</title>
-    <link rel="stylesheet" type="text/css" href="daniel.css"/>
+    <link rel="stylesheet" type="text/css" href="Estilos.css"/>
     <link rel="stylesheet" type="text/css" href="bootstrap.css"/>
   </head>
   <body>
       <div class="container" id="contenedor">
 
-        <div id="titulo">
+        <div class="row">
         <h1 style="color:white; text-align: center">Urban Music</H1>
         </div>
 
+        <div class="row">
+        <?php
+            //FORM SUBMITTED
+            if (isset($_POST["Gmail"])) {
+              //CREATING THE CONNECTION
+              $connection = new mysqli("localhost", "tf", "2asirtriana", "tf");
+              //TESTING IF THE CONNECTION WAS RIGHT
+              if ($connection->connect_errno) {
+                  printf("Connection failed: %s\n", $connection->connect_error);
+                  exit();
+              }
+              //MAKING A SELECT QUERY
+              //Password coded with md5 at the database. Look for better options
+              $consulta="select * from usuario where
+              Gmail='".$_POST["Gmail"]."' and password=md5('".$_POST["password"]."');";
+              //Test if the query was correct
+              //SQL Injection Possible
+              //Check http://php.net/manual/es/mysqli.prepare.php for more security
+              if ($result = $connection->query($consulta)) {
+                  //No rows returned
+                  if ($result->num_rows===0) {
+                    echo "LOGIN INVALIDO";
+                  } else {
+                    //VALID LOGIN. SETTING SESSION VARS
+                    $_SESSION["IdUsuario"]=$_POST["Idusuario"];
+                    $_SESSION["language"]="es";
+                    header("Location: index.php");
+                  }
+              } else {
+                echo "Wrong Query";
+              }
+          }
+        ?>
 
-          <?php if (!isset($_POST["Gmail"])) : ?>
 
             <form method="post" id="formulario_inicio" >
               <fieldset>
-                <legend>registro</legend>
-                <span>Gmail:</span><input type="email" name="Email" required><br>
-                <span>password:</span><input type="password" name="password" required><br>
-                <p><?php
-                echo md5($_POST["password"]);
-
-                ?></p>
-                <p><input type="submit" value="Iniciar Sesion"></p>
-                <p><input type="submit" value="Registarte"></p>
+                <legend>Registro</legend>
+                <span>Gmail:</span><input type="email" name="Email" required id="inputt"><br>
+                <span>password:</span><input type="password" name="password" required id="inputt"><br>
+                <p><input type="submit" value="Iniciar Sesion"></p><br>
+                <a style="color:#D1D1D1"; style="height:200px" href=Registro.php>Registarte si aun no lo Estas</a>
           </form>
 
-        <?php else: ?>
 
-
-
-        <?php endif?>
+</div>
 
       </div>
   </body>
