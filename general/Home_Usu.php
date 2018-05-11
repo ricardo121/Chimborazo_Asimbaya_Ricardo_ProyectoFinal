16f6ffe1f45ba146c0b9f0a3046cfdd4fdf93f42
ricardo <ricardoasimbaya@gmail.com>
2018-05-11 13:57:28 +0200

    Carpeta

--- general/Home_Usu.php
+++ general/Home_Usu.php
@@ -57,49 +57,53 @@
       <div class="row">
       <h1 style="color:white; text-align: center"><?php echo"Bienvenido"." ". $Nombre_usu; ?></H1>
       </div>
+
+
+      <div class="row" style="background-color:white; ">
+
+
+
+
+
+        <nav  role="navigation" style="border-bottom: 1px solid  ">
+  <!-- El logotipo y el icono que despliega el menú se agrupan
+       para mostrarlos mejor en los dispositivos móviles -->
+       <div class="navbar-header">
+         <a style="color:#9d9d9d; " class="navbar-brand" href="#">Pistas</a>
+         <a style="color:#9d9d9d; " class="navbar-brand" href="#">Albums</a>
+         <a class="navbar-brand" href="#">Listas</a>
+       </div>
+       </nav>
+       </div>
       <div class="row">
+        <nav class="navbar navbar-default" role="navigation">
+       <div class="navbar-header">
+         <ul class="nav nav-pills nav-stacked">
+           <li><a href="#">Enlace #1</a></li>
+           <li><a href="#">Enlace #2</a></li>
+         </ul>
+       </div>
+
+  <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+       otro elemento que se pueda ocultar al minimizar la barra -->
+  <!--<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Enlace #1</a></li>
+      <li><a href="#">Enlace #2</a></li>
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Enlace #3</a></li>
+
+    </ul>
+  </div>-->
+  </nav>
 
 
 
 
 
-
-        <table class="table">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Edad</th>
-        </thead>
-
-
-
-
-        <?php
-        $query="SELECT * from Usuarios  WHERE Gmail='$Gmail'";
-
-
-        if ($result = $connection->query($query)) {
-
-
-
-            while($obj = $result->fetch_object()) {
-                //PRINTING EACH ROW
-                echo "<tr>";
-                  echo "<td>".$obj->Nombre."</td>";
-                  echo "<td>".$obj->Edad."</td>";
-                echo "</tr>";
-                $Edad_usu =$obj->Edad;
-                $Nombre_usu = $obj->Nombre;
-
-
-            }
-            //Free the result. Avoid High Memory Usages
-        } //END OF THE IF CHECKING IF THE QUERY WAS RIGHT
-      ?>
-    </table>
       </div>
-
-
       </div>
   </body>
 </html>
