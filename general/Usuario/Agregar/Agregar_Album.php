928bd234ad11ab4b2938d46b65cbf63e3d05ee28
ricardo <ricardoasimbaya@gmail.com>
2018-05-24 19:24:55 +0200

    Carpeta

--- general/Usuario/Agregar/Agregar_Album.php
+++ general/Usuario/Agregar/Agregar_Album.php
@@ -1,10 +1,36 @@
-<!DOCTYPE html>
+<?php
+
+  session_start();
+  include_once("Login_Usu.php");
+  Login();
+
+  $Gmail=$_SESSION["Gmail"];
+
+
+
+?>
+
+ <?php
+
+       //CREATING THE CONNECTION
+       $connection = new mysqli("localhost", "root", "Admin2015", "Proyecto",3316);
+       $connection->set_charset("uft8");
+       //TESTING IF THE CONNECTION WAS RIGHT
+       if ($connection->connect_errno) {
+           printf("Connection failed: %s\n", $connection->connect_error);
+           exit();
+       }
+
+
+?>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passing info with POST and HTML FORMS using a single file.</title>
-    <link rel="stylesheet" type="text/css" href=" bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="Estilo_Usu_Agregar.css"/>
 
   </head>
   <body>
@@ -13,24 +39,22 @@
 
 
 
-      <div class="row">
-      <h1 style="color:black; text-align: center">Añadir Nuevo Album</H1>
+      <div class="row" style="background-color: #ff6d4e;" >
+      <h2 style="color:black; text-align: center">Añadir Nuevo Album</h2>
       </div>
 
-        <div class="row" >
+        <div class="row" style="background-color: #ff6d4e;" >
         <?php if (!isset($_POST["Nombre"])) : ?>
-          <form method="post" role="form">
-         <div class="form-group">
-           <label for="ejemplo_email_1">IdAlbum</label>
+          <form method="post" role="form"   style="margin-left:5%;">
+         <div class="form-group" >
+           <label for="ejemplo_email_1" >IdAlbum</label>
            <input type="text" name="Nombre" class="form-control"
-           placeholder="Introduce Nombre de Album">
+           placeholder="Introduce Nombre de Album" style="width: 50%;">
         </div>
             <button type="submit" class="btn btn-default">Enviar</button>
           </form>
-          <form action='/ricardo/Chimborazo_Asimbaya_Ricardo_ProyectoFinal/general/Usuario/Home_Usu.php' method='post'>
-          <ul class='nav navbar-nav navbar-right'>
-            <li><button type='submit' class='btn btn-default navbar-btn' style='margin-right:15px'>Volver</button></li>
-          </ul>
+          <form action='/ricardo/Chimborazo_Asimbaya_Ricardo_ProyectoFinal/general/Usuario/Home_Usu.php' method='post' >
+            <button type='submit' class='btn btn-default navbar-btn'  style="margin-left:90%;" >Volver</button>
           </form>
 
 
