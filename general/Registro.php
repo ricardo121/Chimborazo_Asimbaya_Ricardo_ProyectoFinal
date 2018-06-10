6c0c92bb71a9cf2bcc074f659c6483c72c61f24d
ricardo <ricardoasimbaya@gmail.com>
2018-06-10 14:18:26 +0200

    Carpeta

--- general/Registro.php
+++ general/Registro.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passing info with POST and HTML FORMS using a single file.</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="Estilos_Registro.css"/>
+    <link rel="stylesheet" type="text/css" href="Estilos_Registros.css"/>
 
   </head>
   <body>
